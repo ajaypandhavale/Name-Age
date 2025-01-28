7b64ba66462ae1adac5854fc4b155d5f0a7d6129
Ajay Pandhavale <aj40135821@wipro.com>
2025-01-28 16:53:41 +0530

    create file for name and age

---
+++ src/Name_Age.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Name_Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+	
+		
+		System.out.println("enter the Name");
+		String name = sc.nextLine();
+	    	 
+		System.out.println("enter the Age");
+		int age= sc.nextInt();
+		
+	    	 
+		System.out.println(" name of the employee "+name);
+		
+		System.out.println(" Age of the employee"+age);
+		
+
+//System.out.println(" updating using branch branch name update1 changing data type as float");
+		
+	}
+
+}
+
+	        
